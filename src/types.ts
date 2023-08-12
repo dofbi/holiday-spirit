4f61ceacb1cd6a74e8c34a3934211bb6af1c5b02
Mamadou Diagne <genova@dofbi.com>
2023-08-12 22:24:25 +0000

    ♻️ refactor (type): supabase

--- src/types.ts
+++ src/types.ts
@@ -9,13 +9,67 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
-      [_ in never]: never
+      groups: {
+        Row: {
+          created_at: string | null
+          created_by: string | null
+          id: string
+          name: string
+        }
+        Insert: {
+          created_at?: string | null
+          created_by?: string | null
+          id?: string
+          name: string
+        }
+        Update: {
+          created_at?: string | null
+          created_by?: string | null
+          id?: string
+          name?: string
+        }
+        Relationships: []
+      }
+      members: {
+        Row: {
+          group_id: string | null
+          id: string
+          name: string
+          selected_by: string | null
+        }
+        Insert: {
+          group_id?: string | null
+          id?: string
+          name: string
+          selected_by?: string | null
+        }
+        Update: {
+          group_id?: string | null
+          id?: string
+          name?: string
+          selected_by?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "members_group_id_fkey"
+            columns: ["group_id"]
+            referencedRelation: "groups"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
     }
     Views: {
       [_ in never]: never
     }
     Functions: {
-      [_ in never]: never
+      draw_name11: {
+        Args: {
+          groupid: string
+          username: string
+        }
+        Returns: string
+      }
     }
     Enums: {
       [_ in never]: never
